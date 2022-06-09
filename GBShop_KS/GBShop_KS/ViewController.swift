747abf0603b57081a1eb97de1d95b6bcc089ff89
KonstantinShmondrik <shmond@mail.ru>
2022-06-09 19:54:41 +0300

    init

---
+++ GBShop_KS/GBShop_KS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GBShop_KS
+//
+//  Created by Константин Шмондрик on 09.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
