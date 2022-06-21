531ef16f43abfc0410b98a03c8b39e469807fc2f
KonstantinShmondrik <shmond@mail.ru>
2022-06-21 19:32:37 +0300

    Добавил классы для изменения данных пользователя

---
+++ GBShop_KS/GBShop_KS/App/BusinessLogic/Network/СhangeUserData/СhangeUserData.swift
@@ -0,0 +1,8 @@
+//
+//  СhangeUserData.swift
+//  GBShop_KS
+//
+//  Created by Константин Шмондрик on 21.06.2022.
+//
+
+import Foundation
---
+++ GBShop_KS/GBShop_KS/App/Entity/СhangeUserDataResult.swift
@@ -0,0 +1,8 @@
+//
+//  СhangeUserDataResult.swift
+//  GBShop_KS
+//
+//  Created by Константин Шмондрик on 21.06.2022.
+//
+
+import Foundation
