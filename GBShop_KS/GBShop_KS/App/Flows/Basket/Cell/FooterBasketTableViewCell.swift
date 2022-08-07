79deba77aa28ba25831ad63da223b799f3f10960
KonstantinShmondrik <shmond@mail.ru>
2022-08-08 00:31:04 +0300

    Добавил футер с кнопкой покой купить и информацией о покупке

--- GBShop_KS/GBShop_KS/App/Flows/Basket/Cell/FooterBasketTableViewCell.swift
+++ GBShop_KS/GBShop_KS/App/Flows/Basket/Cell/FooterBasketTableViewCell.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class FooterTableViewCell: UITableViewCell {
+class FooterBasketTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
