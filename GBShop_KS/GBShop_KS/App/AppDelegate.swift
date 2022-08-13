63ce99b409673ff3d7b20bac703e9517c194ade9
KonstantinShmondrik <shmond@mail.ru>
2022-08-13 18:22:11 +0300

    Добавил Crashlitiks

--- GBShop_KS/GBShop_KS/App/AppDelegate.swift
+++ GBShop_KS/GBShop_KS/App/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        FirebaseApp.configure()
     
 //        let auth = requestFactory.makeAuthRequestFactory()
 //        let regist = requestFactory.makeRegistRequestFactory()
